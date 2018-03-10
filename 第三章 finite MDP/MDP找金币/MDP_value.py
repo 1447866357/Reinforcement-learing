606053c6998dd994ada067b351f12d1404749c64
郑云旗 <32673185+1447866357@users.noreply.github.com>
2018-03-10 14:10:51 +0800

    change1

---
+++ 第三章 finite MDP/MDP找金币/MDP_value.py
@@ -0,0 +1,42 @@
+import numpy
+import random as ran
+ran.seed(0)
+from  MDP import *
+import sys
+
+f_result = open('MDP_result3.txt', 'w')
+sys.stdout = f_result
+
+
+def random_pi():
+    actions = ['n', 'w', 'e', 's']
+    r = int(ran.random() * 4)
+    return actions[r]
+
+def compute_random_pi_state_value():
+    value = [ 0.0 for r in range(9)]
+    num   = 1000000
+    for k in range(1, num):
+        for i in range(1, 6):
+            mdp = MDP()
+            s = i
+            is_terminal = False
+            gamma = 1.0
+            v = 0.0
+            while False == is_terminal:
+                a = random_pi()
+                is_terminal, s, r = mdp.transform(s, a)
+                v += gamma * r
+                gamma *= 0.5
+
+            value[i] = (value[i] * (k - 1) + v) / k
+
+        if k % 10000 == 0:
+            print(value)
+
+    print(value)
+
+compute_random_pi_state_value()
+
+
+
